07a54f1806ba6a8376cfff50bbdc1c69210994c2
vishnuchandrappan <cvishnu70@gmail.com>
2020-01-31 16:42:40 +0530

    added workshops page

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,12 @@
 const burger = document.querySelector('.burger');
 const nav = document.querySelector('.nav-links');
+const sections = document.querySelectorAll('section');
 
 burger.addEventListener("click", () => {
     burger.classList.toggle('burger-active');
     nav.classList.toggle('nav-links-active');
+
+    sections.forEach(section => {
+        section.classList.toggle('active');
+    });
 });
