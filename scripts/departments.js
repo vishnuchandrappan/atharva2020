9a369ec1c0a19151b2e052d02929aad3ded0b0b1
vishnuchandrappan <cvishnu70@gmail.com>
2020-02-12 14:21:44 +0530

    changed location of depts

--- scripts/departments.js
+++ scripts/departments.js
@@ -1,16 +1,36 @@
 const data = [
   {
+    id: 1,
+    code: 'CSEIT',
     name: "Technaura",
     image: "https://picsum.photos/200/300",
     description: "Department of Computer Science & Engineering"
   },
   {
-    name: "Exourtia",
+    id: 2,
+    code: 'EEE',
+    name: "Exousia",
     image: "https://picsum.photos/200/300",
     description: "Lorm ipsum 2"
   },
   {
-    name: "Exourtia",
+    id: 3,
+    code: 'ME',
+    name: "Mexurge",
+    image: "https://picsum.photos/200/300",
+    description: "Lorm ipsum 2"
+  },
+  {
+    id: 4,
+    code: 'EC',
+    name: "Analekta",
+    image: "https://picsum.photos/200/300",
+    description: "Lorm ipsum 2"
+  },
+  {
+    id: 5,
+    code: 'CE',
+    name: "Tranquillum",
     image: "https://picsum.photos/200/300",
     description: "Lorm ipsum 2"
   }
