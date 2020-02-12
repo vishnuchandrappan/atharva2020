7c63ffcd1170a921a122fd9c170b38b55d207960
Aswin S <48542499+aswinsanjeev@users.noreply.github.com>
2020-02-12 15:46:10 +0530

    added mexurge

--- scripts/departments.js
+++ scripts/departments.js
@@ -7,41 +7,29 @@ const data = [
     description: "Department of Computer Science & Engineering"
   },
   {
-<<<<<<< HEAD
     id: 2,
     code: 'EEE',
-=======
->>>>>>> 8aeabc5e82c4ecdbadd565a3cc2fc605642930fa
     name: "Exousia",
     image: "https://picsum.photos/200/300",
     description: "Lorm ipsum 2"
   },
   {
-<<<<<<< HEAD
     id: 3,
     code: 'ME',
-=======
->>>>>>> 8aeabc5e82c4ecdbadd565a3cc2fc605642930fa
     name: "Mexurge",
     image: "https://picsum.photos/200/300",
     description: "Lorm ipsum 2"
   },
   {
-<<<<<<< HEAD
     id: 4,
     code: 'EC',
-=======
->>>>>>> 8aeabc5e82c4ecdbadd565a3cc2fc605642930fa
     name: "Analekta",
     image: "https://picsum.photos/200/300",
     description: "Lorm ipsum 2"
   },
   {
-<<<<<<< HEAD
     id: 5,
     code: 'CE',
-=======
->>>>>>> 8aeabc5e82c4ecdbadd565a3cc2fc605642930fa
     name: "Tranquillum",
     image: "https://picsum.photos/200/300",
     description: "Lorm ipsum 2"
