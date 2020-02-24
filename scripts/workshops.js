de6386d05c9f58535fbcbdaf5749f11f776b0c07
vishnuchandrappan <cvishnu70@gmail.com>
2020-02-24 11:39:13 +0530

    added workshop

--- scripts/workshops.js
+++ scripts/workshops.js
@@ -1,63 +1,33 @@
 //please note : ID for workshop should be given from 1000
 var workshops = [
   {
-    id: 1000,
-    name: "Workshop 1",
-    department: "CSEIT",
-    image: "https://picsum.photos/200/300",
-    description: "Lorm ipsum 1",
-    registration: "100",
-    number1: "Person1 1231231",
-    number2: "Person2 1231232",
-  },
-  {
-    id: 1001,
-    name: "Workshop 2",
-    department: "EC",
-    image: "https://picsum.photos/200/300",
-    description: "Lorm ipsum 2",
-    registration: "150",
-    number1: "Person1 1231231",
-    number2: "Person2 1231232",
-  },
-  {
-    id: 1002,
-    name: "Workshop 3_edited",
-    department: "EEE",
-    image: "https://picsum.photos/200/300",
-    description: "Lorm ipsum 3",
-    registration: "300",
-    number1: "Person1 1231231",
-    number2: "Person2 1231232",
-  },
-  {
-    id: 1003,
-    name: "Workshop 4",
-    department: "MECH",
-    image: "https://picsum.photos/200/300",
-    description: "Lorm ipsum 4",
-    registration: "100",
-    number1: "Person1 1231231",
-    number2: "Person2 1231232",
-  },
-  {
-    id: 1004,
-    name: "Workshop 5",
+    id: 1300,
+    name: `WORKSHOP ON KMBR (KERALA MUNICIPAL BUILDING RULES)`,
     department: "CE",
-    image: "https://picsum.photos/200/300",
-    description: "Lorm ipsum 5",
-    registration: "100",
-    number1: "Person1 1231231",
-    number2: "Person2 1231232",
-  },
-  {
-    id: 1005,
-    name: "Workshop 6",
-    department: "CSEIT",
-    image: "https://picsum.photos/200/300",
-    description: "Lorm ipsum 6",
-    registration: "100",
-    number1: "Person1 1231231",
-    number2: "Person2 1231232",
+    image: "/images/1300.jpg",
+    description: `
+   <h2> Objective:</h2><hr><br> Familiarize with basic KMBR requirements.
+Activities: prepare plan for residential buildings to fit the given plot meeting
+KMBR, Far calculation, Coverage area...<br>
+Total Duration: 6 hours<br>
+Activity points: 18<br>
+8 for workshop certificate<br>
+10 for competition after forenoon session.<br>
+<strong>Prerequisites:</strong> participants must bring their scale and pencil.<br>
+Remarks: suitable for students of all academic years.<br>
+<h2>HIGHLIGHTS</h2><br>
+&nbsp;&nbsp;&nbsp;&nbsp;
+1. All participants shall be awarded participation certificates.<br>
+&nbsp;&nbsp;&nbsp;&nbsp;
+2. Recognition certificate for best performing group during activity.<br>
+&nbsp;&nbsp;&nbsp;&nbsp;
+3. Works on drawings and real-time project.<br>
+&nbsp;&nbsp;&nbsp;&nbsp;
+4. Self-evaluate and build your confidence.<br>
+&nbsp;&nbsp;&nbsp;&nbsp;
+    `,
+    registration: "370/head",
+    number1: "Basith:9400082228",
+    number2: "PHarshana Faisal:7907246466"
   }
 ];
